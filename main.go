85a417cc906bb7d3a33f13fc9cff163d0fbe3ac4
Harish Krishnan <harishkrishnan1993@gmail.com>
2021-10-28 12:33:19 +0530

    Create golang repo

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintf(w, "Hello, world!")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println("Bytes written:", n)
+	})
+}
