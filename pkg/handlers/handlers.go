b0a62803f83dadc87f1326beae8c8ce67e7891e5
Harish Krishnan <harishkrishnan1993@gmail.com>
2021-10-30 22:36:03 +0530

    Pass template data

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/harishkrishnan24/reservation-system/pkg/config"
+	"github.com/harishkrishnan24/reservation-system/pkg/models"
 	"github.com/harishkrishnan24/reservation-system/pkg/render"
 )
 
@@ -29,10 +30,15 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.html")
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
 }
 
 // About is the about page handler
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.html")
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
