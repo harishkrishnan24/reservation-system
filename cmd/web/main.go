ba6c7a8dbf136a576ae46f1ff9af50ab58683413
Harish Krishnan <harishkrishnan1993@gmail.com>
2021-10-30 22:13:26 +0530

    Add template cache

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/harishkrishnan24/reservation-system/pkg/config"
 	"github.com/harishkrishnan24/reservation-system/pkg/handlers"
+	"github.com/harishkrishnan24/reservation-system/pkg/render"
 )
 
 const portNumber = ":8080"
 
 // main is the main application function
 func main() {
+	var app config.AppConfig
 
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Println("Starting application on port", portNumber)
 
